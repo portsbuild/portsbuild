a8885e25c1b9b2f569ab760fe05630216feafa55
Saro <saro@saromedia.com>
2015-10-20 22:19:08 -0400

    cb2: ap2, fpm, dovecot configuration changes

---
+++ directadmin/scripts/fix_ftp.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Source: http://help.directadmin.com/item.php?id=205
+#
+# Usage:
+# chmod 755 fix_ftp.sh
+# ./fix_ftp.sh >> /etc/proftpd.passwd
+# chown root:ftp /etc/proftpd.passwd
+# chmod 640 /etc/proftpd.passwd
+
+PF=/etc/proftpd.passwd
+
+cd /usr/local/directadmin/data/users
+for u in `ls`; do
+{
+          if [ ! -d $u ]; then
+                    continue;
+          fi
+
+          SHADOW=/home/$u/.shadow
+          if [ ! -e $SHADOW ]; then
+                    continue;
+          fi
+
+          #make sure it doesn't already exist
+          COUNT=`grep -c -e "^${u}:" $PF`
+          if [ "$COUNT" -ne 0 ]; then
+                    continue;
+          fi
+
+          UUID=`id -u $u`
+          UGID=`id -g $u`
+
+          echo "${u}:`cat /home/$u/.shadow`:${UUID}:${UGID}:system:/home/${u}:/bin/false";
+
+};
+done;
---
+++ directadmin/scripts/majordomo.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+# Script to install majordomo
+# PortsBuild edition
+
+OS=`uname`
+
+SERVER=http://files.directadmin.com/services/all/majordomo
+ADDPATCHES=1
+
+SOURCEPATH="/usr/local/directadmin/scripts/packages/majordomo-1.94.5"
+
+if [ ! -e ${SOURCEPATH}/Makefile ]
+then
+	echo "The source path for majordomo does not exist. Make sure the correct path is set in majordomo.sh";
+	exit 0;
+fi
+
+/bin/mkdir -p /etc/virtual/majordomo
+
+MDGID=`id -g daemon`
+
+if  [ "$OS" = "FreeBSD" ]; then
+	/usr/sbin/pw useradd majordomo -b /etc/virtual/majordomo -g daemon -s /sbin/nologin 2> /dev/null
+fi
+
+MDUID=`id -u majordomo`
+
+/usr/local/bin/perl -pi -e 's/PERL = .*/PERL = \/usr\/local\/bin\/perl/' ${SOURCEPATH}/Makefile;
+/usr/local/bin/perl -pi -e 's/W_HOME = .*/W_HOME = \/etc\/virtual\/majordomo/' ${SOURCEPATH}/Makefile;
+
+# Perl and Bash weren't getting along. MDUID wasn't showing up so I did it this way.
+STR="/usr/local/bin/perl -pi -e 's/W_USER = .*/W_USER = ${MDUID}/' ${SOURCEPATH}/Makefile";
+eval $STR;
+
+STR="/usr/local/bin/perl -pi -e 's/W_GROUP = .*/W_GROUP = ${MDGID}/' ${SOURCEPATH}/Makefile";
+eval $STR;
+
+STR="/usr/local/bin/perl -pi -e 's/TMPDIR = .*/TMPDIR = \/tmp/' ${SOURCEPATH}/Makefile";
+eval $STR;
+
+
+# Fix REALLY-TO value in digests file
+STR="/usr/local/bin/perl -pi -e 's/\$ARGV\[0\];/\$ARGV\[0\].\${whereami};/' ${SOURCEPATH}/digest";
+eval $STR;
+
+STR="/usr/local/bin/perl -pi -e 's#/usr/test/majordomo#/etc/virtual/majordomo#' ${SOURCEPATH}/sample.cf";
+eval $STR;
+
+cd ${SOURCEPATH};
+
+make wrapper
+make install
+make install-wrapper
+
+/usr/local/bin/perl -pi -e 's#/usr/test/majordomo#/etc/virtual/majordomo#' /etc/virtual/majordomo/majordomo.cf
+
+if [ $ADDPATCHES -eq 0 ]; then
+	exit 0;
+fi
+
+PATCH1=majordomo.patch
+PATCH1_PATH=/etc/virtual/majordomo/${PATCH1}
+if [ ! -s "${PATCH1_PATH}" ]; then
+	wget -O ${PATCH1_PATH} ${SERVER}/${PATCH1}
+fi
+
+if [ -s "${PATCH1_PATH}" ]; then
+	cd /etc/virtual/majordomo
+	patch -p0 < majordomo.patch
+else
+	echo "Cannot find ${PATCH1_PATH} to patch majordomo.";
+fi
+
+ # Just to put up back where we were.. likely not needed.
+cd ${SOURCEPATH};
+
+chmod 750 /etc/virtual/majordomo
+
+exit 0
