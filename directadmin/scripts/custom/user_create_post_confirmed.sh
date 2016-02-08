f3225f6a9d9d965abf5563745401f64684e8b0a8
Saro <saro@sarotech.ca>
2016-02-07 23:33:29 -0500

    Renamed scripts/custom/user_create_post.sh to user_create_post_confirmed.sh to fix exec errors

--- directadmin/scripts/custom/user_create_post_confirmed.sh
+++ directadmin/scripts/custom/user_create_post_confirmed.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 if [ "$spam" = "ON" ]; then
-   DIR="/home/${username}/.spamassassin"
-   mkdir "${DIR}"
-   touch "${DIR}/user_prefs"             #or this is where you'd copy the default user_prefs you want them to have, instead of "touch".
-   chown ${username}:mail "${DIR}"
-   chmod 771 "${DIR}"
-   chown $username:$username "${DIR}/user_prefs"
-   chmod 755 "${DIR}/user_prefs"
-   touch "${DIR}/spam"
-   chown mail:$username "${DIR}/spam"
-   chmod 660 "${DIR}/spam"
+  DIR="/home/${username}/.spamassassin"
+  mkdir "${DIR}"
+  touch "${DIR}/user_prefs"             #or this is where you'd copy the default user_prefs you want them to have, instead of "touch".
+  chown ${username}:mail "${DIR}"
+  chmod 771 "${DIR}"
+  chown $username:$username "${DIR}/user_prefs"
+  chmod 755 "${DIR}/user_prefs"
+  touch "${DIR}/spam"
+  chown mail:$username "${DIR}/spam"
+  chmod 660 "${DIR}/spam"
 fi
 
 ## PB: Restart services after adding a user
