57c5fb6322f717835ae5e9a86e1bbc3752aaf474
Saro Gurunlu <saro@sarotech.ca>
2016-02-18 20:40:58 -0500

    WIP

--- directadmin/scripts/custom/user_create_post_confirmed.sh
+++ directadmin/scripts/custom/user_create_post_confirmed.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
 if [ "$spam" = "ON" ]; then
-  DIR="/home/${username}/.spamassassin"
-  mkdir "${DIR}"
-  touch "${DIR}/user_prefs"             #or this is where you'd copy the default user_prefs you want them to have, instead of "touch".
-  chown ${username}:mail "${DIR}"
-  chmod 771 "${DIR}"
-  chown $username:$username "${DIR}/user_prefs"
-  chmod 755 "${DIR}/user_prefs"
-  touch "${DIR}/spam"
-  chown mail:$username "${DIR}/spam"
-  chmod 660 "${DIR}/spam"
+  DIR=/home/$username/.spamassassin
+  /bin/mkdir $DIR
+  /usr/bin/touch $DIR/user_prefs             # or this is where you'd copy the default user_prefs you want them to have instead of "touch".
+  /usr/sbin/chown ${username}:mail $DIR
+  /bin/chmod 771 $DIR
+  /usr/sbin/chown $username:$username $DIR/user_prefs
+  /bin/chmod 755 $DIR/user_prefs
+  /usr/bin/touch $DIR/spam
+  /usr/sbin/chown mail:$username $DIR/spam
+  /bin/chmod 660 $DIR/spam
 fi
+
 
 ## PB: Restart services after adding a user
 ## (DA can't do these for us right now due to control script filename differences)
+
+# ln -s /usr/local/etc/rc.d/php-fpm /usr/local/etc/rc.d/php-fpm56
+# ln -s /etc/rc.d/named /usr/local/etc/rc.d/named
 
 ## note to self: perhaps add a "sleep 10" here to give DA enough time to do its thing
 ## also add checks to see if php-fpm/apache24 is actually installed (same for nginx)
